2b83a6f214498e2914dcdef035225e87d683372d
Alan Tang <atang42@gmail.com>
2021-05-19 15:53:34 -0700

    Fix docker building script

--- build_images.sh
+++ build_images.sh
@@ -7,12 +7,15 @@ WORK_DIR="$PWD"
 ASSETS_REL_PATH=./assets
 ASSETS_FULL_PATH=${WORK_DIR}/${ASSETS_REL_PATH}
 
+rm -r ${ASSETS_FULL_PATH}
+mkdir ${ASSETS_FULL_PATH}
+
 # Batfish
 echo "Cloning and building batfish"
 
 mvn clean -f projects/pom.xml package
 BATFISH_VERSION=$(grep -1 batfish-parent "projects/pom.xml" | grep version | sed 's/[<>]/|/g' | cut -f3 -d\|)
-cp batfish/projects/allinone/target/allinone-bundle-${BATFISH_VERSION}.jar ${ASSETS_FULL_PATH}/allinone-bundle.jar
-cp -r batfish/questions ${ASSETS_FULL_PATH}
+cp projects/allinone/target/allinone-bundle-${BATFISH_VERSION}.jar ${ASSETS_FULL_PATH}/allinone-bundle.jar
+cp -r questions ${ASSETS_FULL_PATH}
 docker build -f ${WORK_DIR}/batfish.dockerfile -t modnetv/campion --build-arg ASSETS=${ASSETS_REL_PATH} .
 
